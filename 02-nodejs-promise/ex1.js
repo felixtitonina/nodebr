58c825cc81a053e78304b4c9ba4171d0d4a0afe0
felixtitonina <felixtitonina@outlook.com>
2020-03-08 21:59:52 -0300

    2

---
+++ 02-nodejs-promise/ex1.js
@@ -0,0 +1,22 @@
+
+// function fazRequisicao(resolver = true) {
+
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (!resolver){
+//                 reject("Deu erro");
+//                 // ou 
+//                 // throw new Error("Deu erro");
+//             }
+//                 resolve('Promise resolvida')
+//         }, 5000)
+//     })
+// }
+
+// fazRequisicao(false)
+//     .then(console.log)
+//     .catch(console.error);
+let promise1= new Promise ((resolve, reject) => {
+    
+})
+
